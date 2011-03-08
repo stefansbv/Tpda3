18f35f79fe0ef085d12f776b435941086c2908ef
Stefan Suciu <stefbv70@gmail.com>
2011-03-08 15:44:42 +0200

    Layout change in Customers screen

--- lib/Tpda3/Wx/App/Test/Customers.pm
+++ lib/Tpda3/Wx/App/Test/Customers.pm
@@ -43,54 +43,58 @@ sub run_screen {
 
     # TODO: use Wx::Perl::TextValidator
 
-    my $lcustomername = Wx::StaticText->new( $inreg_p, -1, 'Customer', );
+    my $lcustomername = Wx::StaticText->new( $inreg_p, -1, 'Name' );
     my $ecustomername =
-        Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ], );
-
-    my $lcustomernumber = Wx::StaticText->new( $inreg_p, -1, 'Customernumber', );
+        Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ] );
     my $ecustomernumber =
-        Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ], );
+        Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ] );
 
-    my $lcontactlastname = Wx::StaticText->new( $inreg_p, -1, 'Last name', );
+    my $lcontactlastname = Wx::StaticText->new( $inreg_p, -1, 'Last name' );
     my $econtactlastname =
-        Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ], );
+        Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ] );
 
-    my $lcontactfirstname = Wx::StaticText->new( $inreg_p, -1, 'First name', );
+    my $lcontactfirstname = Wx::StaticText->new( $inreg_p, -1, 'First name' );
     my $econtactfirstname =
-        Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ], );
+        Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ] );
 
-    my $lphone = Wx::StaticText->new( $inreg_p, -1, 'Phone', );
-    my $ephone = Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ], );
+    my $lphone = Wx::StaticText->new( $inreg_p, -1, 'Phone' );
+    my $ephone = Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ] );
 
-    my $laddressline1 = Wx::StaticText->new( $inreg_p, -1, 'Address line1', );
-    my $eaddressline1 = Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ], );
+    my $laddressline1 = Wx::StaticText->new( $inreg_p, -1, 'Address line1' );
+    my $eaddressline1 =
+        Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ] );
 
-    my $laddressline2 = Wx::StaticText->new( $inreg_p, -1, 'Address line2', );
-    my $eaddressline2 = Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ], );
+    my $laddressline2 = Wx::StaticText->new( $inreg_p, -1, 'Address line2' );
+    my $eaddressline2 =
+        Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ] );
 
-    my $lcity = Wx::StaticText->new( $inreg_p, -1, 'City', );
-    my $ecity = Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ], );
+    my $lcity = Wx::StaticText->new( $inreg_p, -1, 'City' );
+    my $ecity = Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ] );
 
-    my $lstate = Wx::StaticText->new( $inreg_p, -1, 'State', );
-    my $estate = Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ], );
+    my $lstate = Wx::StaticText->new( $inreg_p, -1, 'State' );
+    my $estate = Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ] );
 
-    my $lcountryname = Wx::StaticText->new( $inreg_p, -1, 'Country', );
-    my $ecountryname = Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ], );
+    my $lcountryname = Wx::StaticText->new( $inreg_p, -1, 'Country' );
+    my $ecountryname =
+        Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ] );
+    my $ecountrycode =
+        Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ] );
 
-    my $lcountrycode = Wx::StaticText->new( $inreg_p, -1, 'cc', );
-    my $ecountrycode = Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ], );
+    my $lsalesrepemployee =
+        Wx::StaticText->new( $inreg_p, -1, 'Sales repres.' );
+    my $esalesrepemployee =
+        Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ] );
 
-    my $lsalesrepemployee = Wx::StaticText->new( $inreg_p, -1, 'Sales repres.', );
-    my $esalesrepemployee = Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ], );
+    my $eemployeenumber =
+        Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ] );
 
-    my $lemployeenumber = Wx::StaticText->new( $inreg_p, -1, 'Emp number', );
-    my $eemployeenumber = Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ], );
+    my $lcreditlimit = Wx::StaticText->new( $inreg_p, -1, 'Credit limit' );
+    my $ecreditlimit =
+        Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ] );
 
-    my $lcreditlimit = Wx::StaticText->new( $inreg_p, -1, 'Credit limit', );
-    my $ecreditlimit = Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ], );
-
-    my $lpostalcode = Wx::StaticText->new( $inreg_p, -1, 'Postal code', );
-    my $epostalcode = Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ], );
+    my $lpostalcode = Wx::StaticText->new( $inreg_p, -1, 'Postal code' );
+    my $epostalcode =
+        Wx::TextCtrl->new( $inreg_p, -1, q{}, [ -1, -1 ], [ -1, -1 ] );
 
     #--- Layout
 
@@ -100,54 +104,57 @@ sub run_screen {
 
     my $loco_mid_sz =
       Wx::StaticBoxSizer->new(
-        Wx::StaticBox->new( $inreg_p, -1, ' Customer ', ), wxVERTICAL, );
+        Wx::StaticBox->new( $inreg_p, -1, ' Customer ', ), wxVERTICAL );
 
-    my $loco_mid_fgs = Wx::FlexGridSizer->new( 2, 2, 5, 10 );
+    my $loco_mid_fgs = Wx::FlexGridSizer->new( 2, 3, 5, 10 );
 
-    $loco_mid_fgs->Add( $lcustomername, 0, wxTOP | wxLEFT,  5 );
-    $loco_mid_fgs->Add( $ecustomername,    0, wxEXPAND | wxTOP, 5 );
-
-    $loco_mid_fgs->Add( $lcustomernumber, 0, wxLEFT,   5 );
-    $loco_mid_fgs->Add( $ecustomernumber, 0, wxEXPAND, 0 );
+    $loco_mid_fgs->Add( $lcustomername,   0, wxLEFT,   5 );
+    $loco_mid_fgs->Add( $ecustomername,   0, wxEXPAND, 5 );
+    $loco_mid_fgs->Add( $ecustomernumber, 0, wxRIGHT,  5 );
 
     $loco_mid_fgs->Add( $lcontactlastname, 0, wxLEFT,   5 );
-    $loco_mid_fgs->Add( $econtactlastname,   0, wxEXPAND, 0 );
+    $loco_mid_fgs->Add( $econtactlastname, 0, wxEXPAND, 0 );
+    $loco_mid_fgs->Add( 5, 0, 0, wxALIGN_CENTER_HORIZONTAL | wxALL, 0 );
 
     $loco_mid_fgs->Add( $lcontactfirstname, 0, wxLEFT,   5 );
-    $loco_mid_fgs->Add( $econtactfirstname,    0, wxEXPAND, 0 );
+    $loco_mid_fgs->Add( $econtactfirstname, 0, wxEXPAND, 0 );
+    $loco_mid_fgs->Add( 5, 0, 0, wxALIGN_CENTER_HORIZONTAL | wxALL, 0 );
 
-    $loco_mid_fgs->Add(  $lphone,    0, wxEXPAND, 0 );
-    $loco_mid_fgs->Add(  $ephone,    0, wxEXPAND, 0 );
+    $loco_mid_fgs->Add( $lphone, 0, wxLEFT,   5 );
+    $loco_mid_fgs->Add( $ephone, 0, wxEXPAND, 0 );
+    $loco_mid_fgs->Add( 5, 0, 0, wxALIGN_CENTER_HORIZONTAL | wxALL, 0 );
 
-    $loco_mid_fgs->Add(  $laddressline1,    0, wxEXPAND, 0 );
-    $loco_mid_fgs->Add(  $eaddressline1,    0, wxEXPAND, 0 );
+    $loco_mid_fgs->Add( $laddressline1, 0, wxLEFT,   5 );
+    $loco_mid_fgs->Add( $eaddressline1, 0, wxEXPAND, 0 );
+    $loco_mid_fgs->Add( 5, 0, 0, wxALIGN_CENTER_HORIZONTAL | wxALL, 0 );
 
-    $loco_mid_fgs->Add(  $laddressline2,    0, wxEXPAND, 0 );
-    $loco_mid_fgs->Add(  $eaddressline2,    0, wxEXPAND, 0 );
+    $loco_mid_fgs->Add( $laddressline2, 0, wxLEFT,   5 );
+    $loco_mid_fgs->Add( $eaddressline2, 0, wxEXPAND, 0 );
+    $loco_mid_fgs->Add( 5, 0, 0, wxALIGN_CENTER_HORIZONTAL | wxALL, 0 );
 
-    $loco_mid_fgs->Add(  $lcity,    0, wxEXPAND, 0 );
-    $loco_mid_fgs->Add(  $ecity,    0, wxEXPAND, 0 );
+    $loco_mid_fgs->Add( $lcity, 0, wxLEFT,   5 );
+    $loco_mid_fgs->Add( $ecity, 0, wxEXPAND, 0 );
+    $loco_mid_fgs->Add( 5, 0, 0, wxALIGN_CENTER_HORIZONTAL | wxALL, 0 );
 
-    $loco_mid_fgs->Add(  $lstate,    0, wxEXPAND, 0 );
-    $loco_mid_fgs->Add(  $estate,    0, wxEXPAND, 0 );
+    $loco_mid_fgs->Add( $lstate, 0, wxLEFT,   5 );
+    $loco_mid_fgs->Add( $estate, 0, wxEXPAND, 0 );
+    $loco_mid_fgs->Add( 5, 0, 0, wxALIGN_CENTER_HORIZONTAL | wxALL, 0 );
 
-    $loco_mid_fgs->Add(  $lcountryname,    0, wxEXPAND, 0 );
-    $loco_mid_fgs->Add(  $ecountryname,    0, wxEXPAND, 0 );
+    $loco_mid_fgs->Add( $lcountryname, 0, wxLEFT,   5 );
+    $loco_mid_fgs->Add( $ecountryname, 0, wxEXPAND, 0 );
+    $loco_mid_fgs->Add( $ecountrycode, 0, wxRIGHT,  5 );
 
-    $loco_mid_fgs->Add(  $lcountrycode,    0, wxEXPAND, 0 );
-    $loco_mid_fgs->Add(  $ecountrycode,    0, wxEXPAND, 0 );
+    $loco_mid_fgs->Add( $lsalesrepemployee, 0, wxLEFT,   5 );
+    $loco_mid_fgs->Add( $esalesrepemployee, 0, wxEXPAND, 0 );
+    $loco_mid_fgs->Add( $eemployeenumber,   0, wxRIGHT,  5 );
 
-    $loco_mid_fgs->Add(  $lsalesrepemployee,    0, wxEXPAND, 0 );
-    $loco_mid_fgs->Add(  $esalesrepemployee,    0, wxEXPAND, 0 );
+    $loco_mid_fgs->Add( $lcreditlimit, 0, wxLEFT,   5 );
+    $loco_mid_fgs->Add( $ecreditlimit, 0, wxEXPAND, 0 );
+    $loco_mid_fgs->Add( 5, 0, 0, wxALIGN_CENTER_HORIZONTAL | wxALL, 0 );
 
-    $loco_mid_fgs->Add(  $lemployeenumber,    0, wxEXPAND, 0 );
-    $loco_mid_fgs->Add(  $eemployeenumber,    0, wxEXPAND, 0 );
-
-    $loco_mid_fgs->Add(  $lcreditlimit,    0, wxEXPAND, 0 );
-    $loco_mid_fgs->Add(  $ecreditlimit,    0, wxEXPAND, 0 );
-
-    $loco_mid_fgs->Add(  $lpostalcode,    0, wxEXPAND, 0 );
-    $loco_mid_fgs->Add(  $epostalcode,    0, wxEXPAND, 0 );
+    $loco_mid_fgs->Add( $lpostalcode, 0, wxLEFT,   5 );
+    $loco_mid_fgs->Add( $epostalcode, 0, wxEXPAND, 0 );
+    $loco_mid_fgs->Add( 5, 0, 0, wxALIGN_CENTER_HORIZONTAL | wxALL, 0 );
 
     #- Layout
 
