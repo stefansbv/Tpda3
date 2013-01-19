e06a73ea5d0237fbeef02e22fe6d76c6f54255d4
Stefan Suciu <stefbv70@gmail.com>
2013-01-19 13:53:38 +0200

    Add CUBRID support, initial import.

---
+++ lib/Tpda3/Db/Connection/Cubrid.pm
@@ -0,0 +1,231 @@
+package Tpda3::Db::Connection::Cubrid;
+
+use strict;
+use warnings;
+
+use Regexp::Common;
+use Log::Log4perl qw(get_logger :levels);
+
+use Tpda3::Exceptions;
+
+use Try::Tiny;
+use DBI;
+
+=head1 NAME
+
+Tpda3::Db::Connection::Cubrid - Connect to a Cubrid database.
+
+=head1 VERSION
+
+Version 0.62
+
+=cut
+
+our $VERSION = 0.62;
+
+=head1 SYNOPSIS
+
+    use Tpda3::Db::Connection::Cubrid;
+
+    my $db = Tpda3::Db::Connection::Cubrid->new();
+
+    $db->db_connect($connection);
+
+
+=head1 METHODS
+
+=head2 new
+
+Constructor
+
+=cut
+
+sub new {
+    my ( $class, $model ) = @_;
+
+    my $self = {};
+
+    $self->{model} = $model;
+
+    bless $self, $class;
+
+    return $self;
+}
+
+=head2 db_connect
+
+Connect to database
+
+=cut
+
+sub db_connect {
+    my ( $self, $conf ) = @_;
+
+    my $log = get_logger();
+
+    my ( $dbname, $host, $port ) = @{$conf}{qw(dbname host port)};
+    my ( $driver, $user, $pass ) = @{$conf}{qw(driver user pass)};
+
+    $log->trace("Database driver is: $driver");
+    $log->trace("Parameters:");
+    $log->trace( " > Database = ", $dbname ? $dbname : '?', "\n" );
+    $log->trace( " > Host     = ", $host   ? $host   : '?', "\n" );
+    $log->trace( " > Port     = ", $port   ? $port   : '?', "\n" );
+    $log->trace( " > User     = ", $user   ? $user   : '?', "\n" );
+
+    my $dsn = qq{dbi:cubrid:database=$dbname;host=$host;port=$port};
+
+    $self->{_dbh} = DBI->connect(
+        $dsn, $user, $pass,
+        {   FetchHashKeyName => 'NAME_lc',
+            AutoCommit       => 1,
+            RaiseError       => 0,
+            PrintError       => 0,
+            LongReadLen      => 524288,
+            HandleError      => sub { $self->handle_error() },
+        }
+    );
+
+    return $self->{_dbh};
+}
+
+=head2 handle_error
+
+Log errors.
+
+=cut
+
+sub handle_error {
+    my $self = shift;
+
+    my $errorstr;
+
+    if ( defined $self->{_dbh} and $self->{_dbh}->isa('DBI::db') ) {
+        $errorstr = $self->{_dbh}->errstr;
+        Tpda3::Exception::Db::SQL->throw(
+            logmsg  => $errorstr,
+            usermsg => $self->parse_error($errorstr),
+        );
+    }
+    else {
+        $errorstr = DBI->errstr;
+        Tpda3::Exception::Db::Connect->throw(
+            logmsg  => $errorstr,
+            usermsg => $self->parse_error($errorstr),
+        );
+    }
+
+    return;
+}
+
+=head2 parse_error
+
+Parse a database error message, and translate it for the user.
+
+RDBMS specific (and maybe version specific?).
+
+=cut
+
+sub parse_error {
+    my ( $self, $cb ) = @_;
+
+    my $log = get_logger();
+
+    print "\nCB: $cb\n\n";
+
+    my $message_type
+        = $cb eq q{} ? "nomessage"
+        : $cb =~ m/Cannot communicate with server/smi   ? "servererror"
+        : $cb =~ m/User ($RE{quoted}) is invalid/smi    ? "username:$1"
+        :                                                 "unknown";
+
+    # Analize and translate
+
+    my ( $type, $name ) = split /:/, $message_type, 2;
+    $name = $name ? $name : '';
+
+    my $translations = {
+        driver      => "fatal#Database driver $name not found",
+        servererror => "fatal#Server not available",
+        username    => "warn#User $name is invalid",
+    };
+
+    my $message;
+    if ( exists $translations->{$type} ) {
+        $message = $translations->{$type};
+    }
+    else {
+        $log->error('EE: Translation error for: $cb!');
+    }
+
+    return $message;
+}
+
+=head2 table_list
+
+Return list of tables from the database.
+
+=cut
+
+sub table_list {
+    my $self = shift;
+    return;
+}
+
+=head2 table_info_short
+
+Table info 'short'.  The 'table_info' method from the Cubrid driver
+doesn't seem to be reliable.
+
+=cut
+
+sub table_info_short {
+    my ( $self, $table ) = @_;
+    return;
+}
+
+=head2 table_keys
+
+Get the primary key field name of the table.
+
+=cut
+
+sub table_keys {
+    my ( $self, $table, $foreign ) = @_;
+    return;
+}
+
+=head2 table_exists
+
+Check if table exists in the database.
+
+=cut
+
+sub table_exists {
+    my ( $self, $table ) = @_;
+    return;
+}
+
+=head1 AUTHOR
+
+Stefan Suciu, C<< <stefan@s2i2.ro> >>
+
+=head1 BUGS
+
+None known.
+
+Please report any bugs or feature requests to the author.
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2010-2012 Stefan Suciu.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation.
+
+=cut
+
+1;    # End of Tpda3::Db::Connection::Cubrid
