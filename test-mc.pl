f8f3158dc632378f8bb1826508445dae6deb64b6
Stefan <stefbv70@gmail.com>
2019-03-29 08:54:50 +0200

    Experiment with a calendar widget
    Based on Tk::MiniCalendar by Lorenz Domke

---
+++ test-mc.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl
+
+use 5.010;
+use strict;
+use warnings;
+
+use Tk;
+
+use Tpda3::Tk::MaxiCalendar;
+
+my $top = MainWindow->new;
+
+my $frm1 = $top->Frame->pack;  # Frame to place MaxiCalendar in
+
+my $minical = $frm1->MaxiCalendar->pack;
+
+my $frm2 = $top->Frame->pack;    # Frame for Ok Button
+my $b_ok = $frm2->Button(
+    -text    => "Ok",
+    -command => sub {
+        my ( $year, $month, $day ) = $minical->date;
+        print "Selected date: $year/$month/$day\n";
+        exit;
+    },
+)->pack;
+
+MainLoop;
