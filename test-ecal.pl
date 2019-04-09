b1c1b21e78864c6a7ffac57007a6e23171f7c6a5
Stefan <stefbv70@gmail.com>
2019-04-09 16:03:29 +0300

    Tweak entry calendar
    Implement select month by moving the label+entryes back and forth;
    And and configure a font for day labels;
    MIsc cleanup and changes;

--- test-ecal.pl
+++ test-ecal.pl
@@ -3,7 +3,7 @@
 use 5.010;
 use strict;
 use warnings;
-use Data::Dump; 
+use Data::Dump;
 
 use Tk;
 
@@ -16,24 +16,48 @@ my $frm1 = $top->Frame->pack;  # Frame to place EntryCalendar in
 
 my $minical = $frm1->EntryCalendar()->pack;
 
-# $minical->select_date(2019, 9, 1);
-$minical->move_first_day(+1);
+# $minical->select_month(2019, 9);
+# $minical->move_first_day(+1);
 
 my $frm2 = $top->Frame->pack;    # Frame for Ok Button
-my $b_ok = $frm2->Button(
-    -text    => "OK",
+
+$frm2->Button(
+    -text    => "<<",
     -command => sub {
-        $minical->move_first_day(+1);
+        $minical->move_first_day(-1);
         my ( $year, $month, $day ) = $minical->date;
         print "Selected date: $year-$month-$day\n";
-        my $eary = $minical->get_entry_array;
+        my $eary = $minical->get_label_entry_array;
         my $poz = 0;
         foreach my $ez ( @{$eary} ) {
-            print "$poz: ", $ez->get, "\n";
+            print "$poz: ", $ez->[1]->get, "\n";
             $poz++;
         }
         # dd $eary;
+        print " index of day #1 = ", $minical->index_of_day(1), "\n";
     },
-)->pack;
+)->pack(-side => 'left');
+
+$frm2->Button(
+    -text    => "Reset",
+    -command => sub {
+        $minical->reset_places();
+    },
+)->pack(-side => 'left');
+
+$frm2->Button(
+    -text    => ">>",
+    -command => sub {
+        $minical->move_first_day(+1);
+        my $eary = $minical->get_label_entry_array;
+        my $poz = 0;
+        foreach my $ez ( @{$eary} ) {
+            print "$poz: ", $ez->[1]->get, "\n";
+            $poz++;
+        }
+        # dd $eary;
+        print " index of day #1 = ", $minical->index_of_day(1), "\n";
+    },
+)->pack(-side => 'left');
 
 MainLoop;
