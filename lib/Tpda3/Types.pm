f0bf87307eae0ac69c5ebf93f9461a52732d58e5
Stefan Suciu <stefbv70@gmail.com>
2020-03-20 16:10:02 +0200

    Add types

--- lib/Tpda3/Types.pm
+++ lib/Tpda3/Types.pm
@@ -7,6 +7,8 @@ use strict;
 use warnings;
 use utf8;
 use Type::Library 0.040 -base, -declare => qw(
+    DateSimple
+    DateRange
     Path
     MailOutlook
     MailOutlookMessage
@@ -17,8 +19,9 @@ use Types::Standard -types;
 # Inherit standard types.
 BEGIN { extends "Types::Standard" };
 
-
 # Other
+class_type DateSimple,         { class => 'Date::Simple' };
+class_type DateRange,          { class => 'Date::Range' };
 class_type Path,               { class => 'Path::Tiny' };
 class_type MailOutlook,        { class => 'Mail::Outlook' };
 class_type MailOutlookMessage, { class => 'Mail::Outlook::Message' };
