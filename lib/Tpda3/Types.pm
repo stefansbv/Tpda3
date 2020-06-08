5986627fd587673670d8f6155bb20e22947bcfda
Stefan Suciu <stefbv70@gmail.com>
2020-06-08 19:45:09 +0300

    Add more types

--- lib/Tpda3/Types.pm
+++ lib/Tpda3/Types.pm
@@ -7,21 +7,23 @@ use strict;
 use warnings;
 use utf8;
 use Type::Library 0.040 -base, -declare => qw(
-    DateRange
-    DateSimple
+    AppLogger
     DBIdb
     DBIxConnector
+    DateRange
+    DateSimple
     ListCompare
     MailOutlook
     MailOutlookMessage
     Path
-    Logger
     TimeMoment
     Tpda3Compare
     Tpda3Config
     Tpda3ConfigConnection
     Tpda3Engine
     Tpda3Hollyday
+    Tpda3Model
+    Tpda3ModelDB
     Tpda3Observable
     Tpda3Record
     Tpda3Target
@@ -39,20 +41,22 @@ class_type DateRange,          { class => 'Date::Range' };
 class_type DBIdb,              { class => 'DBI::db' };
 class_type DBIxConnector,      { class => 'DBIx::Connector' };
 class_type Path,               { class => 'Path::Tiny' };
-class_type Logger              { class => 'Log::Log4perl::Logger' };
+class_type AppLogger,          { class => 'Log::Log4perl::Logger' };
 class_type MailOutlook,        { class => 'Mail::Outlook' };
 class_type MailOutlookMessage, { class => 'Mail::Outlook::Message' };
 class_type TimeMoment,         { class => 'Time::Moment' };
 class_type ListCompare,        { class => 'List::Compare' };
 class_type URIdb,              { class => 'URI::db' };
 
-class_type Tpda3Config,          { class => 'Tpda3::Config' };
-class_type Tpda3ConfigConnection { class => 'Tpda3::Config::Connection' };
-class_type Tpda3Hollyday,        { class => 'Tpda3::Hollyday' };
-class_type Tpda3Observable,      { class => 'Tpda3::Observable' };
-class_type Tpda3Engine ,         { class => 'Tpda3::Engine' };
-class_type Tpda3Target ,         { class => 'Tpda3::Target' };
-class_type Tpda3Compare,         { class => 'Tpda3::Model::Update::Compare' };
-class_type Tpda3Record ,         { class => 'Tpda3::Model::Table::Record' };
+class_type Tpda3Config,           { class => 'Tpda3::Config' };
+class_type Tpda3ConfigConnection, { class => 'Tpda3::Config::Connection' };
+class_type Tpda3Hollyday,         { class => 'Tpda3::Hollyday' };
+class_type Tpda3Model,            { class => 'Tpda3::Model' };
+class_type Tpda3ModelDB,          { class => 'Tpda3::Model::DB' };
+class_type Tpda3Observable,       { class => 'Tpda3::Observable' };
+class_type Tpda3Engine,           { class => 'Tpda3::Engine' };
+class_type Tpda3Target,           { class => 'Tpda3::Target' };
+class_type Tpda3Compare,          { class => 'Tpda3::Model::Update::Compare' };
+class_type Tpda3Record,           { class => 'Tpda3::Model::Table::Record' };
 
 1;
