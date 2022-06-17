4ee5813c4f31fc35d4d645956d62ebf4ccd4a68d
Stefan <stefbv70@gmail.com>
2022-06-17 13:40:17 +0300

    Update types

--- lib/Tpda3/Types.pm
+++ lib/Tpda3/Types.pm
@@ -16,10 +16,13 @@ use Type::Library 0.040 -base, -declare => qw(
     Path
     TimeMoment
     Tpda3Compare
+    Tpda3Contr
+    Tpda3Screen
     Tpda3Config
     Tpda3Hollyday
     Tpda3Record
     Tpda3View
+    TkToplevel
     URIdb
 );
 use Type::Utils -all;
@@ -38,10 +41,13 @@ class_type TimeMoment,         { class => 'Time::Moment' };
 class_type ListCompare,        { class => 'List::Compare' };
 class_type URIdb,              { class => 'URI::db' };
 class_type ExcelWriterXLSX,    { class => 'Excel::Writer::XLSX' };
+class_type TkToplevel,    { class => 'Tk::Toplevel' };
 class_type Tpda3Config,   { class => 'Tpda3::Config' };
 class_type Tpda3Hollyday, { class => 'Tpda3::Hollyday' };
 class_type Tpda3Compare,  { class => 'Tpda3::Model::Update::Compare' };
 class_type Tpda3Record ,  { class => 'Tpda3::Model::Table::Record' };
 class_type Tpda3View ,    { class => 'Tpda3::Tk::View' };
+class_type Tpda3Contr,    { class => 'Tpda3::Tk::Controller' };
+class_type Tpda3Screen,   { class => 'Tpda3::Tk::Screen' };
 
 1;
